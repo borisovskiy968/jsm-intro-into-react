fc43b8e3febc6933bb409e555e5ead2c4ffce4b0
Igor Borisovskiy <58004557+borisovskiy968@users.noreply.github.com>
2022-12-05 19:04:44 +0100

    Create component of MovieCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+
+import MovieCard from './MovieCards';
 
 import './App.css'
 import SearchIcon from './search.svg'
@@ -15,12 +17,13 @@ const movie1 = {
 }
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   }
 
   useEffect(() => {
@@ -45,20 +48,7 @@ const App = () => {
       </div>
 
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-          
-          <div>
-            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title} />
-          </div>
-          
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-        </div>
+        <MovieCard movie1={movie1} />
       </div>
     
     </div>
