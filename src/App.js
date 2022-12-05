cf40f12ab39ee3837b3e5c90665e585a9cddd2fb
Igor Borisovskiy <58004557+borisovskiy968@users.noreply.github.com>
2022-12-05 20:37:22 +0100

    Implement MovieCard into the app and set up movie search

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ const movie1 = {
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -37,20 +38,28 @@ const App = () => {
       <div className="search">
         <input
           placeholder="Search or movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           src={SearchIcon}
           alt="search"
-          onClick={() => {}}
+          onClick={() => searchMovies(searchTerm)}
         />
       </div>
-
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
-    
+      
+      {movies?.length > 0
+        ? (
+          <div className="container">
+            {movies.map((movie) => (
+            <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+      )}
     </div>
   );
 }
